61640e8ec7ec1c855357c560c9d15f6833001af9
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-05 16:31:12 +0700

    telah dibuat bagian 2A

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.text1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
@@ -43,15 +43,15 @@
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // text1
             // 
-            this.button1.Location = new System.Drawing.Point(211, 131);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(57, 45);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.text1.Location = new System.Drawing.Point(211, 131);
+            this.text1.Name = "text1";
+            this.text1.Size = new System.Drawing.Size(57, 45);
+            this.text1.TabIndex = 0;
+            this.text1.Text = "1";
+            this.text1.UseVisualStyleBackColor = true;
+            this.text1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -61,6 +61,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -70,6 +71,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -79,6 +81,7 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "4";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -88,6 +91,7 @@
             this.button5.TabIndex = 4;
             this.button5.Text = "5";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -97,6 +101,7 @@
             this.button6.TabIndex = 5;
             this.button6.Text = "6";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -106,6 +111,7 @@
             this.button7.TabIndex = 7;
             this.button7.Text = "7";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
@@ -115,6 +121,7 @@
             this.button8.TabIndex = 8;
             this.button8.Text = "8";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
@@ -134,6 +141,7 @@
             this.button10.TabIndex = 10;
             this.button10.Text = "0";
             this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // button11
             // 
@@ -159,11 +167,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(217, 77);
+            this.label1.Location = new System.Drawing.Point(208, 84);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 24);
+            this.label1.Size = new System.Drawing.Size(154, 24);
             this.label1.TabIndex = 13;
-            this.label1.Text = "label1";
+            this.label1.Text = "Masukkan Angka";
             // 
             // Form1
             // 
@@ -182,7 +190,7 @@
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.text1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -192,7 +200,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button text1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,11 @@ namespace modul3_103022300088
 {
     public partial class Form1 : Form
     {
+        string angka1;
+        string number;
+
+        private List<int> angkalist = new List<int>();
+
         public Form1()
         {
             InitializeComponent();
@@ -19,22 +24,88 @@ namespace modul3_103022300088
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            number = number + "1";
+            label1.Text = number;
+            label1.Show();
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
-
+            number = number + "9";
+            label1.Text = number;
+            label1.Show();
         }
 
         private void button12_Click(object sender, EventArgs e)
         {
-
+            angka1 = label1.Text;
+            number = "+";
+            
+            Console.WriteLine(angka1);
         }
 
         private void button11_Click(object sender, EventArgs e)
         {
+            int a = Int32.Parse(angka1);
+            int result = Int32.Parse(number);
+            int hasil = a + result;
+            label1.Text = hasil.ToString();
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            number = number + "2";
+            label1.Text = number;
+            label1.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            number = number + "3";
+            label1.Text = number;
+            label1.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            number = number + "4";
+            label1.Text = number;
+            label1.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            number = number + "5";
+            label1.Text = number;
+            label1.Show();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            number = number + "6";
+            label1.Text = number;
+            label1.Show();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            number = number + "7";
+            label1.Text = number;
+            label1.Show();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            number = number + "8";
+            label1.Text = number;
+            label1.Show();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            number = number + "0";
+            label1.Text = number;
+            label1.Show();
         }
     }
 }
